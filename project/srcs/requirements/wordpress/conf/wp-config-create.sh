e53fb4204bd492a460b41134092fd8d4e41cbdc8
alegreci <alegreci@student.42firenze.it>
2023-12-15 18:29:57 +0100

    cli added

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -21,3 +21,26 @@ define( 'WP_REDIS_DATABASE', 0 );
 require_once ABSPATH . 'wp-settings.php';
 EOF
 fi
+
+cd /var/www/
+
+# downloads the WP-CLI PHAR (PHP Archive) file from the GitHub repository. The -O flag tells curl to save the file with the same name as it has on the server.
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+# makes the WP-CLI PHAR file executable.
+chmod +x wp-cli.phar
+
+# moves the WP-CLI PHAR file to the /usr/local/bin directory, which is in the system's PATH, and renames it to wp. This allows you to run the wp command from any directory
+mv wp-cli.phar /usr/local/bin/wp
+
+if ! wp --path=/var/www/ user list --field=user_login | /bin/grep -q "${DB_USER}"; then
+    # wp-administrator user creation
+    wp core install --path=/var/www/ --title=Inception --admin_user=${WP_ADMIN} --admin_password=${WP_PASS} --admin_email=alegre@llll.42.com --url=https://www.alegreci.42.fr
+    # wp-user creation
+    wp user create ${DB_USER} oooo@llll.it --path=/var/www/ --role=author --user_pass=${DB_PASS}
+    wp theme install sonoran --activate --allow-root
+    wp plugin install wp-redis --activate
+    wp wp-redis enable
+fi
+
+/usr/sbin/php-fpm81 -F
